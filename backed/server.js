906f55f51f857847f9ef7da07a057cce182ba194
Zoriba <118100019+zoriba@users.noreply.github.com>
2026-05-15 12:53:31 +0000

    fixed more typos

--- backed/server.js
+++ backed/server.js
@@ -1,6 +1,6 @@
 const { express } = require("express");
-const {cors} from require("cors");
-const {dotenv} from require("dotenv");
+const { cors } = require("cors");
+const { dotenv } = require("dotenv");
 
 dotenv.config();
 
